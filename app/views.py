2a9ed08cd46a5b4671c71084348eb494a327be72
idyakin <i.dyakin@friflex.com>
2024-09-22 12:53:36 +0300

    refactor

--- app/views.py
+++ app/views.py
@@ -1,28 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
 
 from app.models import Kitchen, Grill, Contact
 
 
-# Create your views here.
-
 class IndexView(View):
     def get(self, request, *args, **kwargs):
 
-        kitchens = Kitchen.objects.filter(show_on_site=True)
-        grills = Grill.objects.filter(show_on_site=True)
-        contact = Contact.objects.last()
-        lst = []
-
-        for i in kitchens:
-            # print(i.__dict__)
-            lst.append(i.__dict__)
-
-        for i in grills:
-            print(i.__dict__)
-
-        # print("CONTACT: ", contact.__dict__)
-
-        context = {"contact": contact}
+        context = {
+            "kitchens": Kitchen.objects.filter(show_on_site=True),
+            "grills": Grill.objects.filter(show_on_site=True),
+            "contact": Contact.objects.last()
+        }
         return render(request, "app/index.html", context)
--- grill/settings.py
+++ grill/settings.py
@@ -125,6 +125,11 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = [
+    BASE_DIR
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
